72e78bd92b8ad28f0e4cfe9b9e57f2d107394431
Peter <picusbrit@gmail.com>
2020-10-10 01:36:05 -0400

    Added Sphere veritcal movement

---
+++ Assets/MoveSphere.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSphere : MonoBehaviour
+{
+    public float moveRate = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void MoveUp()
+    {
+        float newY = transform.position.y + moveRate * Time.deltaTime;
+        Vector3 newPos = new Vector3 (transform.position.x, newY, transform.position.z);
+        transform.position = newPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveUp();
+    }
+}
